24ab7ae283480f761716bb18c9c78f955e246ef9
gaelle1701 <gaellecoue@gmail.com>
2022-08-24 17:10:22 +0200

    feat-modals

---
+++ fngMotus/src/app/modals/modals.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-modals',
+  templateUrl: './modals.component.html',
+  styleUrls: ['./modals.component.scss'],
+})
+export class ModalsComponent {
+  public title: string;
+
+  constructor() {}
+
+
+  
+ 
+}
