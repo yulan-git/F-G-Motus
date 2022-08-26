f2adf055491972c782d74db0500f290a69171ccf
Lafferrière <flora.lafferriere@gmail.com>
2022-08-26 09:54:22 +0200

    games rules finished

---
+++ fngMotus/src/app/directives/color-circle.directive.spec.ts
@@ -0,0 +1,8 @@
+import { ColorCircleDirective } from './color-circle.directive';
+
+describe('ColorCircleDirective', () => {
+  it('should create an instance', () => {
+    const directive = new ColorCircleDirective();
+    expect(directive).toBeTruthy();
+  });
+});
